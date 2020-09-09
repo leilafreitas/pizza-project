68c6bf69a90cb26c94a6a733199f923cc47c0294
leilafreitas <leilafreitas159@gmail.com>
2020-09-09 20:25:05 -0300

    atualizado

--- pizzas.js
+++ pizzas.js
@@ -3,7 +3,7 @@ let pizzaJson=[
     {id:2, name:'Cababresa', img:'images/pizza2.png',price:[10.00,15.00,18.00],size:['4','6','8'],description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
     {id:3, name:'Quatro Queijos', img:'images/pizza3.png',price:[10.00,15.00,17.45],size:['4','6','8'],description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
     {id:4, name:'Americana', img:'images/pizza4.png',price:[10.00,15.00,19.77],size:['4','6','8'],description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
-    {id:5, name:'Baiana', img:'images/pizza5.png',price:[10.00,15.00,21.43],size:['4','6','8'],description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
+    {id:5, name:'Toscana', img:'images/pizza5.png',price:[10.00,15.00,21.43],size:['4','6','8'],description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
     {id:6, name:'Moda da Casa', img:'images/pizza6.png',price:[10.00,15.00,18.15],size:['4','6','8'],description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
     {id:7, name:'Chocolate', img:'images/pizza7.png',price:[12.00,15.00,22.36],size:['4','6','8'],description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
 ]
